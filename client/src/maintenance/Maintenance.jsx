6db6b8fc579d457b2003e0e06a26189b190cdd65
Kokila-Kaavian <kokila.ss@kaaviansys.com>
2024-09-10 21:18:43 +0530

    add configuration in mta file for production

--- client/src/maintenance/Maintenance.jsx
+++ client/src/maintenance/Maintenance.jsx
@@ -8,7 +8,7 @@ const Maintenance = () => {
       <div className="maintenance-sub-container">
         <div className="maintenance-content">
           <h1>Application is Under Maintenance</h1>
-          <p className="maintenance-sub-content">We hope to back soon...</p>
+          <p className="maintenance-sub-content">We hope to come back soon...</p>
         </div>
         <div className="maintenance-image">
           <img src="/images/maintenance.png" alt="Maintenance-Image" />
